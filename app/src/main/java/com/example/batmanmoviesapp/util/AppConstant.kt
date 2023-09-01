e38df7958db6c72bc3495b945a17cef56acb8bf4
y.taleghani <y.taleghani@yahoo.com>
2023-09-01 05:31:38 -0700

    add AppConstant.kt object

---
+++ app/src/main/java/com/example/batmanmoviesapp/util/AppConstant.kt
@@ -0,0 +1,7 @@
+package com.example.batmanmoviesapp.util
+
+object AppConstant {
+    const val API_KEY = "3e974fca"
+    const val BASE_URL = "https://www.omdbapi.com/"
+    const val MOVIE_ID = "movieId"
+}
