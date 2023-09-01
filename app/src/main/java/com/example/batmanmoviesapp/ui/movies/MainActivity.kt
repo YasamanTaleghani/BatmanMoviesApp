a779c303684fabea069ac747770ddfffecc169ee
y.taleghani <y.taleghani@yahoo.com>
2023-09-01 05:30:39 -0700

    add view binding and HomeFragment to MainActivity.kt

--- app/src/main/java/com/example/batmanmoviesapp/ui/movies/MainActivity.kt
+++ app/src/main/java/com/example/batmanmoviesapp/ui/movies/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.batmanmoviesapp.ui.movies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.batmanmoviesapp.R
+import com.example.batmanmoviesapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setCurrentFragment(HomeFragment())
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.container, fragment)
+            commit()
+        }
 }
