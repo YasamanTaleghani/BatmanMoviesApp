b4864fd62093eede5dc78077f16e4d37e979cc9e
y.taleghani <y.taleghani@yahoo.com>
2023-09-01 05:31:14 -0700

    update Movie.kt fields

--- app/src/main/java/com/example/batmanmoviesapp/data/model/Movie.kt
+++ app/src/main/java/com/example/batmanmoviesapp/data/model/Movie.kt
@@ -4,13 +4,13 @@ import com.google.gson.annotations.SerializedName
 
 data class Movie (
     @SerializedName("Title")
-    val title: String = "",
+    val title: String? = "",
     @SerializedName("Year")
-    val year: String = "",
+    val year: String? = "",
     @SerializedName("imdbID")
-    val imdbID: String = "",
+    val imdbID: String? = "",
     @SerializedName("Type")
-    val Type: String = "",
+    val type: String? = "",
     @SerializedName("Poster")
-    val Poster: String = "",
+    val poster: String? = "",
 )
